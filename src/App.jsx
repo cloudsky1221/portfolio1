e217058d9e6ca0c25e35a3a6a46f46ca5ae33250
cloudsky1221 <110422625+cloudsky1221@users.noreply.github.com>
2023-06-24 16:56:18 +0530

    Squashed commit of the following:
    commit fdc6b305a35baaebd6465ac982885f1f0a391907
    Author: cloudsky1221 <110422625+cloudsky1221@users.noreply.github.com>
    Date:   Sat Jun 24 16:41:00 2023 +0530
    
        update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 
+import Background from './Components/Background/Background'
+
 function App() {
 
   return (
     <>
     <div className="app"></div>
-    <div className="background"></div>
+    <div className="background">
+      <Background />
+    </div>
     </>
   )
 }
