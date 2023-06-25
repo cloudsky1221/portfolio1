2e879372fef522ff7bc23961d7c7c155cc7525e8
cloudsky1221 <110422625+cloudsky1221@users.noreply.github.com>
2023-06-25 21:43:46 +0530

    Squashed commit of the following:
    commit 45d53ac6f50d0886dab1048d84da5c0170206441
    Author: cloudsky1221 <110422625+cloudsky1221@users.noreply.github.com>
    Date:   Sun Jun 25 21:42:44 2023 +0530
    
        update
    
    commit a09de4e9685fdce2afc6e61d20e6de2e7a2c04a7
    Author: cloudsky1221 <110422625+cloudsky1221@users.noreply.github.com>
    Date:   Sat Jun 24 18:20:13 2023 +0530
    
        update
    
    commit fdc6b305a35baaebd6465ac982885f1f0a391907
    Author: cloudsky1221 <110422625+cloudsky1221@users.noreply.github.com>
    Date:   Sat Jun 24 16:41:00 2023 +0530
    
        update

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import "./Background.css"
 
+import back2 from "./Images/back2.png"
+import back3 from "./Images/back3.png"
+
+const backgroundImageArray = [back2, back3]
+
 function Background() {
+
+  const [backgroundImage, setBackgroundImage] = useState(backgroundImageArray[1])
+
+  function handleBackgroundChange(e) {
+    e.preventDefault();
+    const option = e.target.value;
+    if (option === "city") setBackgroundImage(backgroundImageArray[0]);
+    if (option === "town") setBackgroundImage(backgroundImageArray[1]);
+  }
   return (
-    <div className="canvas">
-      <div className="circle"></div>
-    </div>
+    <>
+    <select name="" id="" onChange={handleBackgroundChange} defaultValue="town">
+      <option value="city">city</option>
+      <option value="town">town</option>
+    </select>
+    <div className="canvas" style={{backgroundImage:`url("${backgroundImage}")`}}></div>
+    </>
   )
 }
 
